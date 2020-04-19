15471c95f23447e57fded6929cdc8f21bface2ad
chtnym9 <46001824+chtnym9@users.noreply.github.com>
2020-04-19 13:27:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,7 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Client disconnected'));
 });
 
-wss.addEventListener('message', event => {  
-  console.log('Received:', event.data);
-      client.send(message);
 
-});
 setInterval(() => {  
   
   wss.clients.forEach((client) => {
